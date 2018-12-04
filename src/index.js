a1934c00963551d8a4a960600251de870e3d77ef
Salma Ahmed Mahmoud <salmaahmedmah@gmail.com>
2018-12-04 17:11:05 +0200

    notifications

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import Notifications from "./Ch-1";
-import DogCart from "./Ch-2";
+import Notifications from "./Ch-1";
+// import DogCart from "./Ch-2";
 // import Counter from "./Ch-3";
 // import Tabs from "./Ch-4";
 // import Clock from "./Ch-5/Clock";
@@ -27,11 +27,11 @@ const App = () => (
     /> */}
     {/* <Clock />
     <Tabs titles={["Home", "About", "Gallery"]} activeIndex={0} />
-    <Counter min={3} max={10} intialCount={10} />*/}
-    <DogCart title="dog" description="cute dog" />
-    {/*<Notifications count={9} />
+    <Counter min={3} max={10} intialCount={10} />
+    <DogCart title="dog" description="cute dog" />*/}
+    <Notifications count={9} />
     <Notifications count={12} />
-    <Notifications count={0} />*/}
+    <Notifications count={0} />
   </>
 );
 
